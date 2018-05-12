b5a9fb560fe40ca059be84b5f589bb48927e2b5c
Matej Trojak <trojak@mail.muni.cz>
2018-05-12 11:25:13 +0200

    Fixed arguments for printFI script

--- printFI.py
+++ printFI.py
@@ -14,7 +14,7 @@ The script requires packages paramiko and scp (both can be installed using pip).
 # print server and username, eventually printer names.
 
 USER = "username"													# user name on the server
-FOLDER = "/home/username/folder/"										# location where files are stored on external adress
+FOLDER = "/home/username/folder/"									# location where files are stored on external adress
 SERVER = "anxur.fi.muni.cz"											# server adress
 PRINTER_SINGLE = "lj4a"												# printer name for simple printing
 PRINTER_DUPLEX = "copy4a-duplex -o sides=two-sided-long-edge"		# printer name for duplex printing
@@ -26,45 +26,24 @@ import sys
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('--file', help='the file to be printed')
-parser.add_argument('--copies', type=int, help='number of copies', default=1)
-parser.add_argument('--duplex', help='choose for duplex', default=False)
+parser.add_argument('file', metavar='file', type=str, help='the file to be printed')
+parser.add_argument('-c', '--copies', type=int, help='number of copies', default=1)
+parser.add_argument('-d', '--duplex', help='duplex printing', action='store_true', default=False)
 
 def error(msg):
 	print(msg)
 	sys.exit(1)
 
-try:
-	from paramiko import SSHClient
-except ImportError:
-	error("Package 'paramiko' is required.")
-try:
-	from scp import SCPClient
-except ImportError:
-	error("Package 'scp' is required.")
-
-"""
-Function handleArguments
-
-Parses given 'arguments'.
-First it tries whether given argument is a number => it is number of copies
-Then it tests whether given argument is one of "s" or "d" and sets Boolean variable duplex accordingly.
-
-return: both number of copies and variable indicating single/duplex print method
-"""
-def handleArguments(arguments):
-	copies = 1
-	duplex = False
-	for arg in arguments:
-		try:
-			copies = int(arg)
-		except ValueError:
-			print("yes")
-			if arg in ["s", "d"]:
-				duplex = True if arg == "d" else False
-			else:
-				raise ValueError("Unknown option '" + arg + "'.")
-	return copies, duplex
+def SSHerrorPrint(ssh_stdin, ssh_stdout, ssh_stderr):
+	print("SSH standard input:")
+	print(ssh_stdin)
+	print("-" * 20)
+	print("SSH standard output:")
+	print(ssh_stdout)
+	print("-" * 20)
+	print("SSH error output:")
+	print(ssh_stderr)
+	print("-" * 20)
 
 """
 Function printFile
@@ -87,29 +66,28 @@ def printFile(file, printer, copies):
 
 	# print the file
 	ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("lpr -P " + printer + " -# " + str(copies) + " " + FOLDER + os.path.basename(file))
-	print(ssh_stderr)
+	SSHerrorPrint(ssh_stdin, ssh_stdout, ssh_stderr)
 
 	sftp.close()
 	ssh.close()
 
 if __name__ == '__main__':
-  args = parser.parse_args()
-  try:
-    from paramiko import SSHClient
-  except ImportError:
-    error("Package 'paramiko' is required.")
-  try:
-    from scp import SCPClient
-  except ImportError:
-    error("Package 'scp' is required.")
+	args = parser.parse_args()
+	try:
+		from paramiko import SSHClient
+	except ImportError:
+		error("Package 'paramiko' is required.")
+	try:
+		from scp import SCPClient
+	except ImportError:
+		error("Package 'scp' is required.")
 
-  if args.copies:
-    copies=args.copies
-  else:
-    copies=1
+	if args.copies:
+		copies=args.copies
+	else:
+		copies=1
 
-  if args.duplex:
-    printFile(args.file, PRINTER_DUPLEX, copies)
-  else:
-    printFile(args.file, PRINTER_SINGLE, copies)
-  	
+	if args.duplex:
+		printFile(args.file, PRINTER_DUPLEX, copies)
+	else:
+		printFile(args.file, PRINTER_SINGLE, copies)
