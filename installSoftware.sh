cd59e38f29b10c650c275d5e7d088f4f825e93da
Matej Trojak <trojak@mail.muni.cz>
2018-08-13 21:01:29 +0200

    Added tool for Google Drive Sync

--- installSoftware.sh
+++ installSoftware.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
-sudo add-apt-repository ppa:ricotz/docky -y && sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y && sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y && sudo add-apt-repository ppa:webupd8team/atom -y && sudo add-apt-repository ppa:gezakovacs/ppa -y && sudo add-apt-repository ppa:wiznote-team -y && sudo add-apt-repository ppa:inkscape.dev/stable -y
+sudo add-apt-repository ppa:ricotz/docky -y && sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y && sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y && sudo add-apt-repository ppa:webupd8team/atom -y && sudo add-apt-repository ppa:gezakovacs/ppa -y && sudo add-apt-repository ppa:wiznote-team -y && sudo add-apt-repository ppa:inkscape.dev/stable -y && sudo add-apt-repository ppa:alessandro-strada/ppa -y
+
 
 sudo apt-get update
 
-sudo apt-get -y install gedit chromium-browser gparted htop texlive-full subversion git plank apcalc xournal steam pinta qbittorrent default-jdk gcc-4.9 nautilus-dropbox wine sublime-text-installer unetbootin python-pip numlockx vim network-manager-openvpn network-manager-openvpn-gnome thefuck evince inkscape
+sudo apt-get -y install gedit chromium-browser gparted htop texlive-full subversion git plank apcalc xournal steam pinta qbittorrent default-jdk gcc-4.9 nautilus-dropbox wine sublime-text-installer unetbootin python-pip numlockx vim network-manager-openvpn network-manager-openvpn-gnome thefuck evince inkscape google-drive-ocamlfuse
 
 # _________________________________________________________________________________________
 # Additional customization & software:
@@ -22,8 +23,4 @@ sudo apt-get -y install gedit chromium-browser gparted htop texlive-full subvers
 # fi
 
 # Unicode stuff:
-# download https://github.com/osense/dotfiles/blob/master/.XCompose
-# and remove first line
-# create .xprofile and put there these lines:
-# export GTK_IM_MODULE='xim'
-# export QT_IM_MODULE='xim'
+# place .XCompose to home
