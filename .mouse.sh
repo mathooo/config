690c2f11c9a699229ad81dd86076214b1872164d
mathooo <trojak@mail.muni.cz>
2018-12-17 09:33:08 +0100

    Script for dell mouse automatic options

---
+++ .mouse.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+xinput --set-prop "pointer:Dell Dell Universal Receiver" "Device Accel Adaptive Deceleration" 2.5
+xinput --set-prop "pointer:Dell Dell Universal Receiver" "Device Accel Velocity Scaling" 1
